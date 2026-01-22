532e69679627ba0da2634f2d13c3c5f4f7d1b092
ankit-nextmariner <Ankit.b@nextmariner.com>
2026-01-22 23:31:41 +0530

    initial setup for the landing hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import { useRef } from 'react';
 
-import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from "./pages";
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+  
 
-export default App;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'/>
+     
+    </main>
+  )
+}
+
+export default App
